78c6123527caa9ccdc8e9a4d0075608879e0e837
Gema Juárez <al315684@uji.es>
2023-04-12 13:46:05 +0200

    Add Toolbar and BottomNavigation

--- app/src/main/java/org/gemapp/myasanaapp/main/ui/theme/Type.kt
+++ app/src/main/java/org/gemapp/myasanaapp/main/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package org.gemapp.appilates.ui.theme
+package org.gemapp.myasanaapp.main.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
---
+++ app/src/main/java/org/gemapp/myasanaapp/settings/SettingsScreen.kt
@@ -0,0 +1,16 @@
+package org.gemapp.myasanaapp.settings
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import org.gemapp.myasanaapp.main.ui.Body1
+
+@Composable
+fun SettingsScreen(navigationController: NavHostController) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Body1(text = "Ajustes")
+    }
+}
